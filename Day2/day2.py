cf1b923f4abdb1482f1c1ffba4522fea0746add7
Irfan Haider <irfanhaiderkhosa56@gmail.com>
2026-07-17 10:43:41 +0500

    Complete Day 2: Python operators and calculator project

---
+++ Day2/day2.py
@@ -0,0 +1,111 @@
+#ARITHMATIC OPERATORS
+num1=15
+num2=4
+print(f"Addition of these numbers ={num1+num2}")
+print(f"Subtraction of these numbers ={num1-num2}")
+print(f"Multiplication of these numbers ={num1*num2}")
+print(f"Division of these numbers ={num1/num2}")
+print(f"Floor Divsion of these numbers ={num1//num2}")
+print(f"Power of these numbers ={num1**num2}")
+
+#Exercise about Arithmatic Operators
+print(20 // 3)
+print(20 % 3)
+print(2 ** 5)
+
+#Comparison Operators
+a=10
+b=5
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#Exercise about Comparison Operators
+print(15 > 20)
+print(10 == 10)
+print(5 != 5)
+
+
+#Assignment Operators
+x=10
+x+=5
+print(x)
+x-=5
+print(x)
+x*=4
+print(x)
+x/=5
+print(x)
+
+
+#Operators Precedence
+result=5+6*2
+print(result)
+result=(5+6)*3
+print(result)
+
+
+#Type () conversion
+num=12
+print(float(num))
+marks=90
+print(str(marks))
+number=45
+print(str(number))
+
+#Mini Project 1 (Simple Calculator)
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+print("====Calculator====")
+print(f"Addition ={num1+num2}")
+print(f"Subtraction={num1-num2}")
+print(f"Multiplication={num1*num2}")
+print(f"Division={num1/num2}")
+print(f"Floor Division={num1//num2}")
+print(f"Modulus={num1%num2}")
+print(f"Power={num1**num2}")
+
+#Practice Exercises
+            #Arithmatic 
+print(27%5)
+print(8**3)
+print(19//4)
+
+            #Comparison
+print(15>10)
+print(8==12)
+print(25<=25)
+
+            #Assignment 
+x=50
+x+=10
+print(x)
+x-=20
+print(x)
+x*=3
+print(x)
+x/=2
+print(x)
+
+
+#Homework (Advanced Calculator)
+name=input("What is your name?")
+first_num=int(input("Enter the first number:"))
+second_num=int(input("Enter the second number:"))
+print(f"=========================Welcome {name}====================")
+print(f"Additon:...{first_num+second_num}")
+
+print(f"Subtraction:...{first_num-second_num}")
+
+print(f"Multiplication:...{first_num*second_num}")
+
+print(f"Division:...{first_num/second_num}")
+
+print(f"Floor Division:...{first_num//second_num}")
+
+print(f"Modulus:...{first_num%second_num}")
+
+print(f"Power:...{first_num**second_num}")

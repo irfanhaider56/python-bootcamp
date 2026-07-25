c43b5526bc986871d1ff35170d74317e728611ef
Irfan Haider <irfanhaiderkhosa56@gmail.com>
2026-07-25 06:14:33 +0500

    Day 5 completed: Functions and mini projects

--- Day5/homework.py
+++ Day5/homework.py
@@ -1,14 +1,28 @@
-#Build a bank account system using functions deposit, withdraw, check balance 
-def deposit(deposit_amount,x):
-    deposit_amount=int(input("Enter the amount you want to deposit: "))
-    x=x+deposit_amount
-    return x
-def withdraw(withdraw_amount,x):
-     withdraw_amount=int(input("Enter the amount you want to withdraw: "))
-     x=x-withdraw_amount
-     return x
-def balance(x):
-    return x
-print(f"Deposit amount is : {deposit(100,1000)}")
-print(f"Withdraw amount is : {withdraw(100,1000)}")
-print(f"Current balance is : {balance(100)}")
+#Make a bank account performing functions deposit, withdraw and check balance
+
+#check balance function
+balance=int(input("Enter the balance: "))
+def check_balance(total_balance):
+    return total_balance
+print(f"Your current balance is : {check_balance(balance)}")
+#deposit amount function
+def deposit(deposit_amount,balance):
+    balance=deposit_amount+balance
+    return balance
+
+#withdraw amount
+def withdraw(withdraw_amount,balance):
+    if withdraw_amount>balance:
+        print("Insufficient balance")
+    else:
+        balance=balance-withdraw_amount
+    return balance
+
+check_balance(balance)
+deposit_amount=int(input("Enter the deposit amount:"))
+balance=deposit(deposit_amount,balance)
+print(f"Balance after deposit: {balance}")
+
+withdraw_amount=int(input("Enter the withdrawal amount:"))
+balance=withdraw(withdraw_amount,balance)
+print(f"Balance after withdrawal money is : {balance}")
